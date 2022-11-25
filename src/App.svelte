bcc50200c62502393c1877bbba45caf4d0457966
felix t <41817193+o2bomb@users.noreply.github.com>
2022-11-26 00:40:41 +0800

    Some buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Button from "./lib/components/Button.svelte";
   import Circle from "./lib/components/Circle.svelte";
   import Sidebar from "./lib/components/Sidebar/Sidebar.svelte";
   import Experience from "./lib/Experience.svelte";
@@ -21,6 +22,12 @@
     <Circle />
     <h1 class="oswald-medium uppercase">Bobby Jones</h1>
     <p>Lorem ipsum asdlfk jasdfkl jlasdkj asdlfk jdlfkads ladksjf dasf.</p>
+    <div>
+      <Button>Projects</Button>
+      <Button>Experience</Button>
+      <Button>Skills</Button>
+      <Button>About</Button>
+    </div>
   </section>
   <Projects />
   <Experience />
---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="typescript">
+</script>
+
+<button class="c-Button">
+  <slot />
+</button>
+
+<style>
+  .c-Button {
+  }
+</style>
