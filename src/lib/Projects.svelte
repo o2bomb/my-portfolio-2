8b62dd79ac2f71b9807e04e254599ab119d20f93
felix t <41817193+o2bomb@users.noreply.github.com>
2022-12-02 18:19:16 +0800

    4 projects with mobile responsiveness

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -12,7 +12,7 @@
         animateCards = true;
       },
       {
-        threshold: 0.7,
+        threshold: 0.5,
       }
     );
 
@@ -22,6 +22,9 @@
 </script>
 
 <section id="cards" class="c-Projects">
+  <div class="header-container">
+    <h2 class="oswald-medium" class:animate={animateCards}>Projects</h2>
+  </div>
   <div class="container">
     <div class="card">
       <Card class="card" initRotX={-20} initRotY={20} animate={animateCards}>
@@ -42,16 +45,7 @@
       </Card>
     </div>
     <div class="card">
-      <Card class="card" initRotX={5} initRotY={0} animate={animateCards}>
-        <div class="aspect large" />
-        <img
-          src="https://pbs.twimg.com/media/FKbdq1LWYAcX11l?format=jpg&name=4096x4096"
-          alt="woman"
-        />
-      </Card>
-    </div>
-    <div class="card">
-      <Card class="card" initRotX={15} initRotY={-20} animate={animateCards}>
+      <Card class="card" initRotX={15} initRotY={20} animate={animateCards}>
         <div class="aspect" />
         <img
           src="https://pbs.twimg.com/media/FfLVrPJUAAEaE8L?format=jpg&name=large"
@@ -60,7 +54,7 @@
       </Card>
     </div>
     <div class="card">
-      <Card class="card" initRotX={10} initRotY={20} animate={animateCards}>
+      <Card class="card" initRotX={10} initRotY={-20} animate={animateCards}>
         <div class="aspect" />
         <img
           src="https://pbs.twimg.com/media/FGj1QqpUYAEDfe4?format=jpg&name=4096x4096"
@@ -75,6 +69,7 @@
   .c-Projects {
     position: relative;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     min-height: 100vh;
@@ -83,13 +78,43 @@
       rgba(0, 0, 0, 0) 0%,
       rgba(0, 0, 0, 1) 100%
     );
+
+    --card-width: 30rem;
+  }
+
+  .header-container {
+    overflow-y: hidden;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  h2 {
+    font: var(--font-oswald-bold);
+    font-size: 10rem;
+    text-transform: uppercase;
+    transform: translateY(100%);
+  }
+
+  h2.animate {
+    animation: slide-up 1s cubic-bezier(0.83, 0, 0.17, 1) forwards;
+  }
+  @keyframes slide-up {
+    0% {
+      transform: translateY(100%);
+    }
+    100% {
+      transform: translateY(0);
+    }
   }
 
   .container {
+    position: relative;
     display: grid;
     grid-template-columns: repeat(
-      auto-fit,
-      minmax(calc(min(30rem, 100vw)), 1fr)
+      2,
+      minmax(calc(min(var(--card-width), 100vw)), 1fr)
     );
     justify-items: center;
     gap: 2rem;
@@ -97,34 +122,31 @@
     max-width: 1400px;
   }
 
+  .card {
+    width: fit-content;
+  }
   .card:nth-child(1) {
     transform: translate(
       calc(max(100vw - 400px, 0px) * 0),
-      calc(max(100vw - 400px, 0px) * 0.03)
+      calc(max(100vw - 400px, 0px) * -0.08)
     );
   }
   .card:nth-child(2) {
     transform: translate(
       calc(max(100vw - 400px, 0px) * 0),
-      calc(max(100vw - 400px, 0px) * 0.03)
+      calc(max(100vw - 400px, 0px) * -0.08)
     );
   }
   .card:nth-child(3) {
     transform: translate(
-      calc(max(100vw - 400px, 0px) * 0.22),
-      calc(max(100vw - 400px, 0px) * 0)
+      calc(max(100vw - 400px, 0px) * 0),
+      calc(max(100vw - 400px, 0px) * 0.08)
     );
   }
   .card:nth-child(4) {
     transform: translate(
       calc(max(100vw - 400px, 0px) * 0),
-      calc(max(100vw - 400px, 0px) * 0.2)
-    );
-  }
-  .card:nth-child(5) {
-    transform: translate(
-      calc(max(100vw - 400px, 0px) * 0),
-      calc(max(100vw - 400px, 0px) * 0)
+      calc(max(100vw - 400px, 0px) * 0.08)
     );
   }
 
@@ -140,10 +162,49 @@
   }
 
   .aspect {
-    width: calc(min(20rem, 100vw));
+    width: calc(min(var(--card-width), 100vw));
     padding-bottom: 56.25%;
   }
-  .aspect.large {
-    width: calc(min(30rem, 100vw));
+
+  @media (max-width: 1000px) {
+    h2 {
+      font-size: 6rem;
+    }
+
+    .container {
+      grid-template-columns: repeat(2, minmax(calc(min(20rem, 100vw)), 1fr));
+    }
+    .aspect {
+      width: calc(min(20rem, 100vw));
+      padding-bottom: 56.25%;
+    }
+  }
+
+  @media (max-width: 700px) {
+    .header-container {
+      position: unset;
+      transform: none;
+    }
+    h2 {
+      font: var(--font-oswald-medium);
+      font-size: 3rem;
+      text-transform: uppercase;
+      padding: 2rem;
+      transform: translateY(100%);
+    }
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      width: auto;
+    }
+    .card {
+      transform: none !important;
+    }
+
+    .aspect {
+      width: calc(min(30rem, 100vw));
+      padding-bottom: 56.25%;
+    }
   }
 </style>
