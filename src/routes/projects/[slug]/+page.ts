5c097d10541d4761832542653021aa02a3502681
felix t <41817193+o2bomb@users.noreply.github.com>
2022-12-10 00:33:29 +0800

    Prefect

--- src/routes/projects/[slug]/+page.ts
+++ src/routes/projects/[slug]/+page.ts
@@ -1,4 +1,9 @@
 import type { PageLoad } from "./$types";
+
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+export const prerender = true;
+
 export const load: PageLoad = ({ params }) => {
     return {
         project: PROJECT_DATA[params.slug],
