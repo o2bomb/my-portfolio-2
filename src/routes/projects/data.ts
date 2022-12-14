a33b0aae137f04bfe3e932041efd77ac96bae94d
felix t <41817193+o2bomb@users.noreply.github.com>
2022-12-15 01:26:44 +0800

    Fix invalid export

---
+++ src/routes/projects/data.ts
@@ -0,0 +1,97 @@
+interface Project {
+    id: string;
+    title: string;
+    company?: string;
+    description: string;
+    gallery:
+        | {
+              src: string;
+              alt?: string;
+              is_video?: false;
+          }[]
+        | {
+              src: string;
+              alt?: never;
+              is_video: true;
+          }[];
+    prev?: string;
+    next?: string;
+}
+
+type ProjectData = {
+    [id: string]: Project;
+};
+
+export const PROJECT_DATA: ProjectData = {
+    xsyn: {
+        id: "xsyn",
+        title: "XSYN",
+        company: "Ninja Syndicate",
+        description: `
+        <ul>
+        <li>Built the 3D backdrop of this site. The backdrop can be interacted with and showcases all current and planned future projects belonging to the XSYN universe, each game represented by a planet</li>
+        </ul>
+        <i>Technologies used: React, TypeScript, Next.js, Three.js</i>
+        `,
+        gallery: [
+            {
+                src: "/my work/xsyn/1.png",
+            },
+        ],
+        next: "supremacy",
+    },
+    supremacy: {
+        id: "supremacy",
+        title: "Supremacy: Battle Arena",
+        company: "Ninja Syndicate",
+        description: `
+        <ul>
+        <li>Supremacy: Battle Arena is a rolling 24/7 livestream of gigantic mechs fighting in an endless battle royale, supported by their owners and assisted by the spectators via the web-app</li>
+        <li>Updates from the Battle Arena are sent to the React app in real-time via multiple WebSocket connections</li>
+        <li>Built user interface and server-side functionality for various core features such as the dutch auction player abilities system, player inbox, and player-owned mech loadouts</li>
+        <li>Designed and built user interface for the battle lobbies page</li>
+        </ul>
+        <i>Technologies used: React, TypeScript, MUI, WebSockets, RESTful API, Go, Postgres, Docker</i>
+        `,
+        gallery: [
+            {
+                src: "/my work/supremacy/1.png",
+            },
+        ],
+        prev: "xsyn",
+        next: "weareninja",
+    },
+    weareninja: {
+        id: "weareninja",
+        title: "We Are Ninja",
+        company: "Ninja Syndicate",
+        description: `
+        <ul>
+        <li>Designed and built the 3D backdrop of this site</li>
+        <li>Built the "Team" section. It dynamically pulls data from Prismic, a headless CMS</li>
+        </ul>
+        <i>Technologies used: React, TypeScript, Next.js, Three.js, Prismic (headless CMS)</i>
+        `,
+        gallery: [
+            {
+                src: "/my work/weareninja/1.png",
+            },
+        ],
+        prev: "supremacy",
+        next: "art",
+    },
+    art: {
+        id: "art",
+        title: "Art Showcase 2020",
+        description: `
+        <p>A website that I built to showcase all of my favourite art pieces from 2020. I experimented with mouse hover effects and custom cursor effects. The website also features fully animated page transitions, which I built from scratch (no external libraries used).</p>
+        <i>Technologies used: HTML, CSS, TypeScript</i>
+        `,
+        gallery: [
+            {
+                src: "/my work/art/1.png",
+            },
+        ],
+        prev: "weareninja",
+    },
+};
